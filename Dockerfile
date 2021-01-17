c6cfab5fca6a25f22a33fd836f6aa202c0274053
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2021-01-17 21:50:42 +0200

    feat(conf): add .env.example; update .gitignore; add war file feat(servlet): now ConnectionPoolContextListener take parameters for db from env vars

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM tomcat:jdk11-openjdk
 LABEL maintainer="pspos.developqkation@gmail.com"
 
 RUN rm -rf /usr/local/tomcat/webapps/ROOT
-
 COPY build/libs/4-lab-1.0-SNAPSHOT.war /usr/local/tomcat/webapps/ROOT.war
 
 EXPOSE 8080
--- src/main/java/com/kpi/lab4/servlets/config/ConnectionPoolContextListener.java
+++ src/main/java/com/kpi/lab4/servlets/config/ConnectionPoolContextListener.java
@@ -3,6 +3,8 @@ package com.kpi.lab4.servlets.config;
 import com.kpi.lab4.dao.ConnectionPool;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -11,20 +13,34 @@ import javax.sql.DataSource;
 
 @WebListener("Creates a connection pool")
 public class ConnectionPoolContextListener implements ServletContextListener {
+    private static Logger logger = LogManager.getLogger(ConnectionPoolContextListener.class);
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         if (ConnectionPool.getDataSource() == null) {
+            String dbName = System.getenv("DB_NAME");
+            String cloudSqlInstance = System.getenv("CLOUD_SQL_INSTANCE");
+            String username = System.getenv("DB_USERNAME");
+            String password = System.getenv("DB_PASSWORD");
+            logger.debug("Database config:");
+            logger.debug(dbName);
+            logger.debug(cloudSqlInstance);
+            logger.debug(username);
+            logger.debug(password);
+
             HikariConfig config = new HikariConfig();
             config.setDriverClassName("org.postgresql.Driver");
-            config.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/java_lab");
-            config.setUsername("postgres");
-            config.setPassword("postgres");
+            config.setJdbcUrl(String.format("jdbc:postgresql:///%s", dbName));
+            config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory");
+            config.addDataSourceProperty("cloudSqlInstance", cloudSqlInstance);
+            config.addDataSourceProperty("ipTypes", "PUBLIC,PRIVATE");
+            config.setUsername(username);
+            config.setPassword(password);
             config.addDataSourceProperty("cachePrepStmts" , "true");
             config.addDataSourceProperty("prepStmtCacheSize" , "250");
             config.addDataSourceProperty("prepStmtCacheSqlLimit" , "2048");
             config.setConnectionTimeout(20000);
-            config.setMaximumPoolSize(20);
+            config.setMaximumPoolSize(50);
             DataSource dataSource = new HikariDataSource(config);
             ConnectionPool.setDataSource(dataSource);
         }
